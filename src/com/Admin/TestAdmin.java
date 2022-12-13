70e1e966f116e6f17afd5c1c66760dfc68a3cb2d
VaishnaviSawant8634 <vaishnavisawant721@gmail.com>
2022-12-13 11:43:49 +0530

    added new project

---
+++ src/com/Admin/TestAdmin.java
@@ -0,0 +1,12 @@
+package com.Admin;
+
+import java.sql.SQLException;
+
+public class TestAdmin {
+
+	public static void main(String[] args) throws SQLException {
+		Verify vr=new Verify();
+		 vr.InputLogin();
+	}
+}
+
